49a8492cd1f51455b60082ea3d29dcb84c06f1e8
Konstantin Komelin <konstantin.komelin@gmail.com>
2017-04-08 15:30:43 +0300

    Created a console command for the string extraction.

---
+++ src/Console/ExtractCommand.php
@@ -0,0 +1,67 @@
+<?php
+namespace KKomelin\TranslatableStringExtractor\Console;
+
+use Illuminate\Console\Command;
+use KKomelin\TranslatableStringExtractor\Extractor;
+use Symfony\Component\Console\Input\InputArgument;
+
+class ExtractCommand extends Command {
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'translations:extract';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Extract translatable strings for a language to a JSON file.';
+
+
+    /**
+     * @var Extractor
+     */
+    protected $extractor;
+
+    /**
+     * ExtractCommand constructor.
+     *
+     * @param Extractor $extractor
+     */
+    public function __construct(Extractor $extractor)
+    {
+        parent::__construct();
+        
+        $this->extractor = $extractor;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function fire()
+    {
+        $language = $this->argument('lang');
+
+        $this->extractor->extract($language);
+
+        $this->info('Translatable strings have been extracted and written to the ' . $language . '.json file.');
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return array(
+            array('lang', InputArgument::REQUIRED, 'A language code for which the translatable strings are extracted, e.g. "es".'),
+        );
+    }
+}
---
+++ src/Extractor.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace KKomelin\TranslatableStringExtractor;
+
+class Extractor
+{
+    /**
+     * Extract tanslatable strings from the project files and record them to the JSON language file.
+     *
+     * @param $language
+     */
+    public function extract($language) {
+
+    }
+}
